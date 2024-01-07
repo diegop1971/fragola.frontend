7b043e58490489fdbf8c129fe814fff949fb09f9
diegop <diego.puccio@gmail.com>
2024-01-07 09:12:24 -0300

    restitui ErrorHandlingService.ts de manera temporal

---
+++ app/shared/application/ErrorHandlingService.ts
@@ -0,0 +1,28 @@
+import router from '@/router'
+import axios from 'axios';
+
+class ErrorHandlingService
+{
+    constructor(){}
+
+    public async handleApiError(error:any) {
+        console.log('eepsss', error)
+        if (axios.isAxiosError(error)) {
+            if (error.response) {
+                if (error.response.status === 401) {
+                    router.push({ name: 'login' });
+                }
+                return error.response.data;
+            } else if (error.request) {
+                return `Mensaje de error: ${error}`;
+                //console.error('No se recibió una respuesta del servidor!!!!!!!!!!!!');
+            } else {
+                //console.error('Error al configurar la solicitud!!!!!!!!!!!!!!!!!!!!!:', error.message);
+                return `Mensaje de error: ${error}`;
+            }
+        } else {
+            return `Mensaje de error: ${error}`
+        }
+    }
+}
+export default ErrorHandlingService;
